b904bdc2d69f8d2aef6e80a5064631a94fc7a13f
otopoto0019 <reimu10marisa3410@gmail.com>
2024-06-13 02:30:02 +0900

    openaiにポスリクしたらそのapi_keyと時刻をsqliteに記録するようにした flutterアプリからapiにアクセスできるようにcorsを有効にした

--- lib/app.py
+++ lib/app.py
@@ -4,11 +4,14 @@ import string
 import openai
 from flask import Flask, request, jsonify
 from flask_argon2 import Argon2
+from flask_cors import CORS
 
-from lib.sqlite.SqliteHandller import init_sqlite, insert_user, isExistedUUID, is_valid_api_key
+from lib.sqlite.SqliteHandller import init_sqlite, insert_user, is_existed_uuid, is_valid_api_key, \
+    insert_usage
 
 app = Flask(__name__)
 argon2 = Argon2(app)
+CORS(app)
 
 app.config["JWT_SECRET_KEY"] = "".join(random.choice("0123456789abcdefghijklmnopqrstuvwxyz") for _ in range(64))
 openai_client = openai.OpenAI(api_key="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")
@@ -20,7 +23,7 @@ def generate_api_key():
 
 def require_api_key(func):
     def wrapper(*args, **kwargs):
-        api_key = request.headers.get("api_key")
+        api_key = request.headers.get("api-key")
         if not api_key or not is_valid_api_key(api_key, argon2):
             return jsonify({"status": "error", "message": "this api key is empty or invalid"}), 401
         return func(*args, **kwargs)
@@ -37,13 +40,13 @@ def register():
     data = request.get_json()
     uuid = data["uuid"]
 
-    if isExistedUUID(uuid):
+    if is_existed_uuid(uuid):
         return jsonify({"status": "error", "message": "this uuid has already registered"}), 400
 
     api_key = generate_api_key()
     hashed_api_key = argon2.generate_password_hash(api_key)
     insert_user(uuid, hashed_api_key)
-    return jsonify({"status": "success", "api_key": api_key})
+    return jsonify({"status": "success", "api_key": api_key}), 201
 
 
 @app.route("/generate_response", methods=["POST"])
@@ -55,14 +58,19 @@ def generate_response():
     if prompt == "":
         return jsonify({"status": "error", "message": "prompt should not be empty"}), 400
 
-    response = openai_client.completions.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt=prompt,
-        max_tokens=50,
-        temperature=0.1
-    )
+    try:
+        response = openai_client.completions.create(
+            model="gpt-3.5-turbo-instruct",
+            prompt=prompt,
+            max_tokens=50,
+            temperature=0.4
+        )
+        response_text = response.choices[0].text.replace("\n", "")
+        insert_usage(response_text, request.headers.get("api-key"), argon2)
+        return jsonify({"status": "success", "text": response_text}), 201
 
-    return jsonify({"status": "success", "text": response.choices[0].text})
+    except Exception as e:
+        return jsonify({"status": "error", "message": e.__str__()}), 400
 
 
 @app.before_request
--- lib/sqlite/SqliteHandller.py
+++ lib/sqlite/SqliteHandller.py
@@ -1,5 +1,4 @@
 import sqlite3
-import trace
 
 from flask_argon2 import Argon2
 
@@ -25,7 +24,7 @@ def create_tables():
         "CREATE TABLE IF NOT EXISTS usage ("
         "id INTEGER PRIMARY KEY,"
         "user_id INTEGER,"
-        "usage_count INTEGER DEFAULT 0,"
+        "response TEXT,"
         "last_used_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,"
         "FOREIGN KEY (user_id) REFERENCES user (id)"
         ")"
@@ -43,16 +42,31 @@ def insert_user(uuid, api_key):
     return
 
 
-def getIdFromUUID(uuid):
+def insert_usage(response, api_key, argon2: Argon2):
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
-    cur.execute("SELECT id FROM user WHERE uuid = ?", (uuid,))
-    result = cur.fetchall()
+    user_id = get_user_id_from_api_key(api_key, argon2)
+    cur.execute("INSERT INTO usage (user_id, response) VALUES (?, ?)", (user_id, response))
+    conn.commit()
     conn.close()
-    return result[0][0]
+    return
 
 
-def isExistedUUID(uuid):
+def get_user_id_from_api_key(api_key, argon2: Argon2):
+    if not is_valid_api_key(api_key, argon2):
+        return -1
+
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM user")
+    result = cur.fetchall()
+
+    for user in result:
+        if argon2.check_password_hash(user[2], api_key):
+            return user[0]
+
+
+def is_existed_uuid(uuid):
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
     cur.execute("SELECT * FROM user WHERE uuid = ?", (uuid,))
